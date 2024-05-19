7746ab721ae7921d99d5045eab2bae5df9eb2cbe
RandomJapaneseDude <83206060+inoue-773@users.noreply.github.com>
2024-05-19 22:48:33 +0900

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,3 +6,7 @@ description 'Greenscreen MLO created by Michno, modified by Nickyboy'
 version '7.7.3'
 
 this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'GreenScreenSINC.ytyp'
+
+files{ 'GreenScreenSINC.ytyp' }
